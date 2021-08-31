68bfc81123880f31e6c8be845e0086c247c3dc93
dtuchs <d.tuchs@propellerads.net>
2021-08-31 21:22:15 +0300

    Added JUnit5

---
+++ src/test/java/guru/qa/DemoQATest.java
@@ -0,0 +1,40 @@
+package guru.qa;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class DemoQATest {
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("Before all tests!\n");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("After all tests!\n");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("Before test!\n");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("After test!\n");
+    }
+
+    @Test
+    void firstTest() {
+        System.out.println("I love you QA.GURU\n");
+    }
+
+    @Test
+    void secondTest() {
+        System.out.println("I love you QA.GURU, second test!\n");
+    }
+}
